39f75e87802557a55b58792ef7735adab722d02d
= <sk8withaguitar@gmail.com>
2018-05-12 22:13:46 -0700

    Create basic user creation and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ActionController::API
   def test
   end
+
+  #
+  def sign_in_or_create
+    success = false
+    message = ""
+    user_id = nil
+    dataToReturn = {}
+    user = User.find_by_email(params[:email].downcase)
+    if user
+      if user.valid_password?(params[:password])
+        success = true
+      else
+        success = false
+        message = "Invalid password"
+      end
+    else
+      user = User.create(email: params[:email].downcase,
+                  password: params[:password],
+                  password_confirmation: params[:password])
+      message = "Successfully created account"
+      success = true
+    end
+    @dataToReturn = {
+      success: success,
+      message: message,
+      user_id: user.try(:id),
+    }
+  end
+
 end
---
+++ app/views/users/sign_in_or_create.json.jbuilder
@@ -0,0 +1,3 @@
+json.success @dataToReturn[:success]
+json.message @dataToReturn[:message]
+json.user_id @dataToReturn[:user_id]
