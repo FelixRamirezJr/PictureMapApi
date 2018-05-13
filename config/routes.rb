c4aba2390d1292edd3e806369c430fb889230e24
= <sk8withaguitar@gmail.com>
2018-05-13 09:46:43 -0700

    Change sign_in_or_create to a post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "users/sign_in_or_create" => "users#sign_in_or_create", as: :sign_in_or_create
+  post "users/sign_in_or_create" => "users#sign_in_or_create", as: :sign_in_or_create
   get "/test" => "users#test", as: :test
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
